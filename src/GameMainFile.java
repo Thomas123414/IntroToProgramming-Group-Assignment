153b940058bbf4f0c76825a5c4457805faf72065
Dih-ain <s4165917@student.rmit.edu.au>
2025-04-16 09:14:49 +1000

    edits

--- src/GameMainFile.java
+++ src/GameMainFile.java
@@ -2,6 +2,9 @@ package src;
 
 public class GameMainFile {
     public static void main(String[] args) {
-        
+        System.out.println("HELLO");
+        System.out.println("DERIN");
+        System.out.println("NEED");
+        System.out.println("JOB");
     }
 }
