16260764e8530b62cdc2e354a21d7cf116addb79
Derin Boratav <derin@boratav.com>
2025-04-16 09:14:48 +1000

    file edit test

--- src/GameMainFile.java
+++ src/GameMainFile.java
@@ -2,6 +2,9 @@ package src;
 
 public class GameMainFile {
     public static void main(String[] args) {
-        
+        System.out.print("helloooo");
+        System.out.print("heloooo");
+        System.out.print("helloooo1");
+        System.out.print("helloooo2");
     }
 }
