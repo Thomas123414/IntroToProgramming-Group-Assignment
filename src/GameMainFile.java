621679cf6a8beee50abfe56e3fbc6ae51b451b2d
ThomasA <s4170673@student.rmit.edu.au>
2025-04-16 09:25:28 +1000

    Starting the main file fresh
    reversing all the testing

--- src/GameMainFile.java
+++ src/GameMainFile.java
@@ -2,10 +2,6 @@ package src;
 
 public class GameMainFile {
     public static void main(String[] args) {
-        System.out.println("HELLO");
-        System.out.println("DERIN");
-        System.out.println("NEED");
-        System.out.println("JOB");
-
+        System.out.println("Derin and Dihain");
     }
 }
